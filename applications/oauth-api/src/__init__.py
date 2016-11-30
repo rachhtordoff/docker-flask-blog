d95f7540ead15a54a5cc03fefa126df9fed4938f
rachael.tordoff <rachael.tordoff@digital.landregistry.gov.uk>
2016-11-30 18:50:21 +0000

    oauth facebook login works

--- applications/oauth-api/src/__init__.py
+++ applications/oauth-api/src/__init__.py
@@ -1,8 +1,54 @@
-from flask import Flask
+from flask import Flask, url_for, request, session
+from flask_oauthlib.client import OAuth
 
 app = Flask(__name__)
+
+SECRET_KEY = 'development key'
+DEBUG = True
+FACEBOOK_APP_ID = '1239932412696644'
+FACEBOOK_APP_SECRET = 'be4160f4485e2e1a9cb3e226cf801f3b'
+
+app.debug = DEBUG
+app.secret_key = SECRET_KEY
+oauth = OAuth()
+
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': 'email'}
+)
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "hey"
+
+
+@app.route('/login')
+def login():
+    return facebook.authorize(callback=url_for('facebook_authorized',
+        next=request.args.get('next') or request.referrer or None,
+        _external=True))
+
+
+@app.route('/login/authorized')
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['oauth_token'] = (resp['access_token'], '')
+    me = facebook.get('/me')
+    return 'Logged in as id=%s name=%s redirect=%s' % \
+        (me.data['id'], me.data['name'], request.args.get('next'))
+
+
+@facebook.tokengetter
+def get_facebook_oauth_token():
+    return session.get('oauth_token')
