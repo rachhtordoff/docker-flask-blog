8495add040e63c7277e489001f3b52b069fb56f5
rachael.tordoff <rachael.tordoff@digital.landregistry.gov.uk>
2016-12-01 19:48:02 +0000

    added logout session pop

--- applications/flaskapp/src/views.py
+++ applications/flaskapp/src/views.py
@@ -1,5 +1,5 @@
-from src import app
-from flask import render_template
+from src import app, facebook
+from flask import render_template, url_for, request, session
 
 @app.route('/')
 @app.route('/index')
@@ -8,3 +8,41 @@ def index():
     return render_template('index.html',
                            title='Home',
                            user=user)
+
+@app.route('/login')
+def login():
+    return facebook.authorize(callback=url_for('facebook_authorized',
+        next=request.args.get('next') or request.referrer or None,
+        _external=True))
+
+
+@app.route('/login/authorized')
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['logged_in'] = True
+    session['oauth_token'] = (resp['access_token'], '')
+    me = facebook.get('/me')
+    return 'Logged in as id=%s name=%s redirect=%s' % \
+        (me.data['id'], me.data['name'], request.args.get('next'))
+
+
+@facebook.tokengetter
+def get_facebook_oauth_token():
+    return session.get('oauth_token')
+
+def pop_login_session():
+    session.pop('logged_in', None)
+    session.pop('oauth_token', None)
+    if session.get('oauth_token') is None:
+        return "success"
+    return "unsuccess"
+
+@app.route("/logout")
+def logout():
+    logout = pop_login_session()
+    return logout
